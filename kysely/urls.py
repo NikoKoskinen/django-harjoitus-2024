6c692db2d89adca5450c511b6877566b082aefbb
NikoKoskinen <47375@edu.raseko.fi>
2024-01-29 16:15:43 +0200

    viimeisimmät muutokset

--- kysely/urls.py
+++ kysely/urls.py
@@ -3,6 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # esim: /kyselyt/
     path("", views.index, name="index"),
-
+    # esim: /kyselyt/5/
+    path("<int:question_id>/", views.näytä, name="näytä"),
+    # esim: /kyselyt/5/tulokset/
+    path("<int:question_id>/tulokset/", views.tulokset, name="tulokset"),
+    # esim: /kysely/5/äänestä/
+    path("<int:question_id>/äänestä/", views.äänestä, name="äänestä"),
 ]
--- kysely/views.py
+++ kysely/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+from .models import Kysymys
 
 
 def index(request):
-    return HttpResponse("Heippatirallaa. olet kysely-apin index-sivulla.")
+    kysymyslista = Kysymys.objects.order_by("-julkaisupvm")[:2]
+    context = {
+        "kysymykset": kysymyslista,
+    }
+    return render(request, "kysely/index.html", context)
+
+
+def näytä(request, question_id):
+    return HttpResponse(f"Katsot juuri kysymystä {question_id}")
+
+
+def tulokset(request, question_id):
+    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia")
+
+
+def äänestä(request, question_id):
+    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
