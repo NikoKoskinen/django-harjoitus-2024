a9f6526de39a8eba094a8f5882e02873b7c3c937
NikoKoskinen <47375@edu.raseko.fi>
2024-02-05 15:32:31 +0200

    5.2.2024 iltapäivän muutokset

--- kysely/urls.py
+++ kysely/urls.py
@@ -2,13 +2,14 @@ from django.urls import path
 
 from . import views
 
+app_name = "kysely"
 urlpatterns = [
-    # esim: /kyselyt/
-    path("", views.index, name="index"),
-    # esim: /kyselyt/5/
+    # esim. /kyselyt/
+    path("", views.indeksi, name="indeksi"),
+    # esim. /kyselyt/5/
     path("<int:kysymys_id>/", views.näytä, name="näytä"),
-    # esim: /kyselyt/5/tulokset/
-    path("<int:question_id>/tulokset/", views.tulokset, name="tulokset"),
-    # esim: /kysely/5/äänestä/
-    path("<int:question_id>/äänestä/", views.äänestä, name="äänestä"),
+    # esim. /kyselyt/5/results/
+    path("<int:kysymys_id>/tulokset/", views.tulokset, name="tulokset"),
+    # esim. /kyselyt/5/äänestä/
+    path("<int:kysymys_id>/äänestä/", views.äänestä, name="äänestä"),
 ]
--- kysely/views.py
+++ kysely/views.py
@@ -1,26 +1,45 @@
-from django.http import Http404, HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
-from django.shortcuts import render
-
-from .models import Kysymys
+from django.urls import reverse
 
 
-def index(request):
+from .models import Kysymys, Vaihtoehto
+
+
+def indeksi(request):
     kysymyslista = Kysymys.objects.order_by("-julkaisupvm")[:2]
     context = {
         "kysymykset": kysymyslista,
     }
-    return render(request, "kysely/index.html", context)
+    return render(request, "kysely/indeksi.html", context)
 
 
 def näytä(request, kysymys_id):
-   kysymys = get_object_or_404(Kysymys, pk=kysymys_id)
-   return render(request, "kysely/näytä.html", {"kysymys": kysymys})
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    return render(request, "kysely/näytä.html", {"kysymys": kysym})
 
 
-def tulokset(request, question_id):
-    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia")
+def tulokset(request, kysymys_id):
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    return render(request, "kysely/tulokset.html", {"kysymys": kysym})
 
 
-def äänestä(request, question_id):
-    return HttpResponse(f"Olet äänestämässä kysymykseen {question_id}")
+def äänestä(request, kysymys_id):
+    kysym = get_object_or_404(Kysymys, pk=kysymys_id)
+    try:
+        valittu = kysym.vaihtoehto_set.get(pk=request.POST["valittu"])
+    except (KeyError, Vaihtoehto.DoesNotExist):
+        # Näytä kysymyslomake uudelleen
+        return render(
+            request,
+            "kysely/näytä.html",
+            {
+                "kysymys": kysym,
+                "virheviesti": "Et valinnut mitään vaihtoehtoa.",
+            },
+        )
+    else:
+        valittu.ääniä += 1
+        valittu.save()
+        osoite = reverse("kysely:tulokset", args=(kysym.id,))
+        return HttpResponseRedirect(osoite)
