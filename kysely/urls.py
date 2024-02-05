fb30baccc9adb9589876a0895d7abb7fa871c0c3
NikoKoskinen <47375@edu.raseko.fi>
2024-02-05 15:58:36 +0200

    2.5.2024 iltapäivä korjaus urls

--- kysely/urls.py
+++ kysely/urls.py
@@ -5,11 +5,15 @@ from . import views
 app_name = "kysely"
 urlpatterns = [
     # esim. /kyselyt/
-    path("", views.ListaNäkymä.as_views(), name="indeksi"),
+    path("", views.ListaNäkymä.as_view(), name="indeksi"),
     # esim. /kyselyt/5/
-    path("<int:pk>/", views.näytä, name="näytä"),
-    # esim. /kyselyt/5/results/
-    path("<int:pk>/tulokset/", views.TuloksetNäkymä.as_view(), name="tulokset"),
+    path("<int:pk>/", views.NäytäNäkymä.as_view(), name="näytä"),
+    # esim. /kyselyt/5/tulokset/
+    path(
+        "<int:pk>/tulokset/",
+        views.TuloksetNäkymä.as_view(),
+        name="tulokset",
+    ),
     # esim. /kyselyt/5/äänestä/
     path("<int:kysymys_id>/äänestä/", views.äänestä, name="äänestä"),
 ]
